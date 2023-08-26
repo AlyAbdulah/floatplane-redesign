e350505ead17267c052ce4d91ccf58218a19a201
AlyAbdullah <accounts@alyabdulah.com>
2023-08-26 15:14:01 +0500

    Added Missin Detail on Support Page.

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,4 +1,5 @@
 import { FAQContent } from "@/components/faq-content";
+import { EnvelopeIcon } from "@heroicons/react/20/solid";
 
 export default function Support() {
   return (
@@ -7,6 +8,16 @@ export default function Support() {
           Frequently Asked Questions
         </h1>
         <FAQContent />
+        <div className="px-1">
+          <span>Need help and can&apos;t find an answer in the FAQ section? &nbsp;</span>
+          <button className="text-white bg-primary hover:bg-primary/75 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center mr-2">
+            <EnvelopeIcon className="w-4 h-4 mr-2 text-white" />
+            Contact Support
+          </button>
+          <div className="mt-2">
+            <button className="text-white bg-primary hover:bg-primary/75 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 focus:outline-none">Become a creator on Floatplane</button>
+          </div>
+        </div>
     </>
   );
 }
